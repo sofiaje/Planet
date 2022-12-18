cadebf69132233b7f4bed1309500a0db0c988565
sofiaje <sofia.jespersen@hotmail.com>
2022-12-18 18:27:38 +0100

    icons

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const primaryNav = document.querySelector(".primary-nav");
 const navToggle = document.querySelector(".mobile-nav-toggle");
-
+const icon = document.querySelector(".fa-solid");
+console.log(icon)
 
 navToggle.addEventListener("click", () => {
     const visibility = primaryNav.getAttribute("data-visible");
+    icon.classList.toggle("fa-bars");
+    icon.classList.toggle("fa-xmark");
 
     if (visibility === "false") {
         primaryNav.setAttribute("data-visible", true)
